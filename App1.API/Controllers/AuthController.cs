1deb820cab8a2feebd43a6b7abb3f97db3df5691
basel68 <baselfarouk09@gmail.com>
2024-12-28 20:11:12 +0200

    changing apis permissions

--- App1.API/Controllers/AuthController.cs
+++ App1.API/Controllers/AuthController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace App1.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("admin/[controller]")]
     [ApiController]
     public class AuthController : Controller
     {
